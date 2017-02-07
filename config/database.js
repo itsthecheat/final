88d18cea19757a9f1f7e004542b5ca6640fe4432
Leslie Behum <llaurenb@gmail.com>
2017-02-07 16:44:28 -0500

    db

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var uri = 'mongodb://heroku_3l38vkt2:u5p08rh9d2mfpb4li7lv0mrodq@ds139969.mlab.com:39969/heroku_3l38vkt2';
 
-  mongoose.connect('mongodb://localhost:27017/users_p04' || uri);
+  mongoose.connect(uri || 'mongodb://localhost:27017/users_p04' );
 
 
